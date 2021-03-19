ee53da34caca0ce6bce4a157ef1f4d526cc0c6bd
craigrusselltiu <craigrusselltiu@users.noreply.github.com>
2021-03-19 22:57:58 +1100

    Added all icons for lists

--- src/images/index.js
+++ src/images/index.js
@@ -1,14 +1,40 @@
+import assembly from './icons/assembly.png'
+import aws from './icons/aws.png'
+import bitbucket from './icons/bitbucket.png'
+import bootstrap from './icons/bootstrap.png'
+import c from './icons/c.png'
 import code from './icons/code.png'
+import cpp from './icons/cpp.png'
+import csharp from './icons/csharp.png'
+import css from './icons/css.png'
 import eye from './icons/eye.png'
 import github from './icons/github.png'
 import gmail from './icons/gmail.png'
+import hibernate from './icons/hibernate.png'
+import html from './icons/html.png'
+import java from './icons/java.png'
+import javascript from './icons/javascript.png'
+import junit from './icons/junit.png'
 import linkedin from './icons/linkedin.png'
+import lua from './icons/lua.png'
+import matlab from './icons/matlab.png'
+import maven from './icons/maven.png'
+import mongodb from './icons/mongodb.png'
+import mysql from './icons/mysql.png'
+import neo4j from './icons/neo4j.png'
 import noeye from './icons/noeye.png'
 import python from './icons/python.png'
+import r from './icons/r.png'
 import react from './icons/react.png'
 import replay from './icons/replay.png'
 import resume from './icons/resume.png'
+import selenium from './icons/selenium.png'
+import spring from './icons/spring.png'
+import sql from './icons/sql.png'
+import tomcat from './icons/tomcat.png'
+import unittest from './icons/unittest.png'
 import video from './icons/video.png'
+import vscode from './icons/vscode.png'
 
 import p_beer from './projects/beer.png'
 import p_coffee from './projects/coffee.png'
@@ -20,17 +46,43 @@ import p_website from './projects/website.png'
 import p_yoshi from './projects/yoshi.png'
 
 export {
+    assembly,
+    aws,
+    bitbucket,
+    bootstrap,
+    c,
     code,
+    cpp,
+    csharp,
+    css,
     eye,
     github,
     gmail,
+    hibernate,
+    html,
+    java,
+    javascript,
+    junit,
     linkedin,
+    lua,
+    matlab,
+    maven,
+    mongodb,
+    mysql,
+    neo4j,
     noeye,
     python,
+    r,
     react,
     replay,
     resume,
+    selenium,
+    spring,
+    sql,
+    tomcat,
+    unittest,
     video,
+    vscode,
     p_beer,
     p_coffee,
     p_pvz,
--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,12 +1,15 @@
 import TextCenter from '../components/TextCenter'
+import glasses from '../images/glasses.png'
 
 const Contact = () => {
     return (
         <>
             <TextCenter
                 header='CONTACT'
-                text=""
+                text="If you would like to reach out and have a chat with me, just shoot me an email at craigrusselltiu@gmail.com or click the email button on the bar towards the left side of the page.
+                    I look forward to speaking with you soon!"
             />
+
         </>
     )
 }
