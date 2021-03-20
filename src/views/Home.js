c58b6ea2cf34a72008c712c2302f5334f64f1ebe
craigrusselltiu <craigrusselltiu@users.noreply.github.com>
2021-03-21 04:25:45 +1100

    Update about contents

--- src/views/Home.js
+++ src/views/Home.js
@@ -48,7 +48,7 @@ const Home = () => {
         <>
             <TextPhotoPair
                 header='MORE THAN JUST A CODE MONKEY'
-                text="Hey! My name's Craig and I'm a software engineer. I'm a passionate programmer, people person, problem solver, and a naturally techy guy —
+                text="Hey! My name's Craig and I'm a passionate software engineer. I'm a people person, problem solver, and a naturally techy guy —
                     I mean, it's kind of hard not to be when you've grown up with computers everywhere.
                     Technology has always and will always pique my interest, but at the end of the day I'm just a regular guy who happens to love coding. (Hover over the polaroids!)"
                 img={pic1}
