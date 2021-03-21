097e7c537b0f34f8082c61a4d14c4c204d7ce46a
craigrusselltiu <craigrusselltiu@users.noreply.github.com>
2021-03-21 16:07:01 +1100

    Update all web form fields to be required fields

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
 
     function sendEmail(e) {
         e.preventDefault();
-    
+        
         emailjs.sendForm('gmail', 'craigrusselltiu', e.target, 'user_xZVaYClL2uZByoDkgCRUY').then((result) => {
             console.log(result.text);
         }, (error) => {
@@ -32,10 +32,10 @@ const Contact = () => {
                     <div className='content'>
                         <form className="contact-form" onSubmit={sendEmail}>
                             <h1 className='txt'>EMAIL FORM</h1>
-                            <input className='form' type="text" placeholder="Name" name="name" /><br />
-                            <input className='form' type="email" placeholder="E-mail" name="email" /><br />
-                            <input className='form' type="text" placeholder="Subject" name="subject" /><br />
-                            <textarea className='form' placeholder="Message To Craig" name="message" style={{height: '200px'}} /><br />
+                            <input className='form' type="text" placeholder="Name" name="name" required /><br />
+                            <input className='form' type="email" placeholder="E-mail" name="email" required /><br />
+                            <input className='form' type="text" placeholder="Subject" name="subject" required /><br />
+                            <textarea className='form' placeholder="Message To Craig" name="message" required style={{height: '200px'}} /><br />
                             <input className='form-btn' type="submit" value="Send" /><br />
                             <p className='txt'>{text}</p>
                         </form>
