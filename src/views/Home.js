62dd5b38fdfbc0a6878c9e38ff024efad98aab22
craigrusselltiu <craigrusselltiu@users.noreply.github.com>
2021-04-04 19:56:10 +1000

    Update about me

--- src/views/Home.js
+++ src/views/Home.js
@@ -47,7 +47,8 @@ const Home = () => {
                 header='MORE THAN JUST A CODE MONKEY'
                 text="Hey! My name's Craig and I'm a passionate software engineer. I'm a people person, problem solver, and a naturally techy guy —
                     I mean, it's kind of hard not to be when you've grown up with computers everywhere.
-                    Technology has always and will always pique my interest, but at the end of the day I'm just a regular guy who happens to love coding. (Hover over the polaroids!)"
+                    Technology has always and will always pique my interest, but at the end of the day I'm just a regular guy who happened to completely fall in love with coding.
+                    (Hover over the polaroids!)"
                 img={pic1}
                 sig={'Osaka, 2018'}
             />
